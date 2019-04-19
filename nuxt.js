216b1e425f642c3687130e26226793abc177f22c
monkey <34064683+monkey01127@users.noreply.github.com>
2019-04-19 12:23:01 +0800

    Create nuxt.js

---
+++ nuxt.js
@@ -0,0 +1,16 @@
+Nuxt.js 使用 Webpack 和 vue-loader 、 babel-loader 来处理代码的自动化构建工作（如打包、代码分层、压缩等等）。
+
+SASS、LESS、 Stylus
+
+基于 Vue.js
+自动代码分层
+服务端渲染
+强大的路由功能，支持异步数据
+静态文件服务
+ES6/ES7 语法支持
+打包和压缩 JS 和 CSS
+HTML头部标签管理
+本地开发支持热加载
+集成ESLint
+支持各种样式预处理器： SASS、LESS、 Stylus等等
+支持HTTP/2 推送
